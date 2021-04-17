fe70968fd7dd1e404f13d76663241ae76fcdd0f9
edwina1114 <ta517152139@gmail.com>
2021-04-17 22:35:59 +0800

    #註解更完整

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -14,7 +14,7 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
-                R.layout.fragment_title,container,false)
+                R.layout.fragment_title, container, false)
 
         binding.playButton.setOnClickListener {  view: View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)    //根據action的動作來作切換
@@ -32,6 +32,14 @@ class TitleFragment : Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.options_menu,menu)
     }
+
+    /***
+     *Tip: Make sure that the ID of the menu item that you just added is
+     * exactly the same as the ID of the AboutFragment
+     * that you added in the navigation graph. T
+     * his will make the code for the onClick handler much simpler.
+     * 各個menu的id一定要對應navigation path裡面需要之fragment的id
+     */
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
